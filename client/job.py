52296a4d390c7984c78acb3a66076a341216b79f
Lukáš Doktor <ldoktor@redhat.com>
2012-05-22 16:24:47 +0200

    client.job: Add default test locations to html_report (colored pages patch)
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- client/job.py
+++ client/job.py
@@ -967,7 +967,10 @@ class base_client_job(base_job.base_job):
 
         # write out a job HTML report
         try:
-            html_report.create_report(self.resultdir)
+            # TODO: add test_filenames here
+            test_dirs = [os.path.join(self.clientdir, 'tests/kvm/tests')]
+            test_dirs.append(os.path.join(self.clientdir, 'virt/tests'))
+            html_report.create_report(self.resultdir, test_filenames=test_dirs)
         except Exception, e:
             logging.error("Error writing job HTML report: %s", e)
 
